b2d2843595a72ff0d87bebb4f679980502cae14c
Dhruv-patel-1702 <111880388+Dhruv-patel-1702@users.noreply.github.com>
2023-11-01 01:16:56 +0530

    Pract-8

---
+++ MAD_PRACTICAL8_21012011071/app/src/main/java/com/example/mad_practical8_21012011071/AlarmService.kt
@@ -0,0 +1,24 @@
+package com.example.mad_practical8_21012011071
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+import com.example.mad_practical8_21012011071.R
+
+class AlarmService : Service() {
+    lateinit var player : MediaPlayer
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent != null) {
+            player = MediaPlayer.create(this, R.raw.alarm)
+            player.start()
+        }
+        return START_STICKY
+    }
+    override fun onDestroy() {
+        player.stop()
+        super.onDestroy()
+    }
+}
